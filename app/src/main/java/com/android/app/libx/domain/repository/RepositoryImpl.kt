297a3a343072324deb9d6b685a9e9f25c328db19
navneet <nav700neet@gmail.com>
2025-03-12 00:03:31 +0530

    rest api

---
+++ app/src/main/java/com/android/app/libx/domain/repository/RepositoryImpl.kt
@@ -0,0 +1,22 @@
+package com.android.app.libx.domain.repository
+
+import com.android.app.libx.data.api.ApiService
+import com.android.app.libx.data.models.login.LoginRequest
+import com.android.app.libx.data.models.register.RegisterRequest
+import com.android.app.libx.data.models.register.RegisterResponse
+import com.android.app.libx.data.models.user.User
+import com.android.app.libx.domain.repository.Repository
+import retrofit2.Response
+import javax.inject.Inject
+
+class RepositoryImpl @Inject constructor(
+    private val apiService: ApiService
+) : Repository {
+    override suspend fun login(request: LoginRequest): Response<User> {
+        return apiService.login(request)
+    }
+
+    override suspend fun register(request: RegisterRequest): Response<RegisterResponse> {
+        return apiService.register(request)
+    }
+}
